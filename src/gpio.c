1972a2ec5412a101b0f6dc40a3f4f562df0b7277
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 08:00:05 -0300

    Update gpio.c

--- src/gpio.c
+++ src/gpio.c
@@ -1,3 +1,5 @@
+#include "gpio.h"
+
 // Todos os endereços úteis do módulo de gpio
 #define GPIO1			0x4804C000		// GPIO1 registers
 #define GPIO_IRQSTATUS		(*(volatile unsigned int *)GPIO1 + 0x2C)
@@ -7,15 +9,6 @@
 #define GPIO_IRQSTATUS_SET	(*(volatile unsigned int *)GPIO1 + 0x34)
 #define GPIO_IRQSTATUS_CLR	(*(volatile unsigned int *)GPIO1 + 0x3C)
 #define GPIO_RISINGDETECT	(*(volatile unsigned int *)GPIO1 + 0x148)
-
-// Protótipos das funções
-void gpio1_rising_detect(int pin);
-void gpio1_ack_interrupt(int pin);
-void gpio1_interrupt_disable(int pin);
-void gpio1_interrupt_enable(int pin);
-void gpio1_output_enable(int pin);
-void gpio1_cleardataout(int pin);
-void gpio1_setdataout(int pin);
 
 // As funções propriamente ditas
 void gpio1_rising_detect(int pin){
