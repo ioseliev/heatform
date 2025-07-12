81d897886cf97c7914a1b746b72c1d1b883b217e
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 11:26:17 -0300

    Update cmper.c

--- src/cmper.c
+++ src/cmper.c
@@ -1,10 +1,5 @@
 #include "cmper.h"
 
-#define CM_PER			0x44E00000		// Clock Module Peripheral Registers
-#define CM_PER_GPIO1_CLKCTRL	(*(volatile unsigned int *)(CM_PER + 0xAC))
-#define CM_PER_I2C2_CLKCTRL	(*(volatile unsigned int *)(CM_PER + 0x44))
-#define CM_PER_I2C1_CLKCTRL	(*(volatile unsigned int *)(CM_PER + 0x48))
-
 // As funções
 void i2c1_clock_enable(void){
 	CM_PER_I2C1_CLKCTRL = 0x2;
