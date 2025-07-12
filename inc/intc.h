c5f23a96a317adbf88592359672f4ddedd349aa8
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 11:31:50 -0300

    Update intc.h

--- inc/intc.h
+++ inc/intc.h
@@ -1,6 +1,12 @@
 #ifndef INTC_H
 #define INTC_H
 
+#define INTC			0x48200000		// Interrupt Controller Registers
+#define INTC_SIR_IRQ		(*(volatile unsigned int *)(INTC + 0x40))
+#define INTC_CONTROL		(*(volatile unsigned int *)(INTC + 0x48))
+#define INTC_MIR_CLEAR2		(*(volatile unsigned int *)(INTC + 0xC8))
+#define INTC_MIR_CLEAR3		(*(volatile unsigned int *)(INTC + 0xE8))
+
 // Protótipo das funções
 void intc_interrupt_rtc_accept(void);
 void intc_interrupt_i2c_accept(void);
