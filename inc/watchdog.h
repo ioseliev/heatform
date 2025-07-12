f205e793d68e7a56cf3f16323e95b01513a2424d
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 11:37:55 -0300

    Update watchdog.h

--- inc/watchdog.h
+++ inc/watchdog.h
@@ -1,5 +1,9 @@
 #ifndef WATCHDOG_H
-#define
+#define WATCHDOG_H
+
+#define WDT_BASE              (*(volatile unsigned int*)0x44E35000)
+#define WDT_WSPR              (*(volatile unsigned int*)0x44E35048)
+#define WDT_WWPS              (*(volatile unsigned int*)0x44E35034)
 
 // Protótipo das funções
 void disable_wdt(void);
