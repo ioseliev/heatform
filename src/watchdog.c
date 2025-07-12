60d01f63bc06abfd5967640df652d5d1cb8f6279
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 11:38:24 -0300

    Update watchdog.c

--- src/watchdog.c
+++ src/watchdog.c
@@ -1,9 +1,5 @@
 #include "watchdog.h"
 
-#define WDT_BASE              (*(volatile unsigned int*)0x44E35000)
-#define WDT_WSPR              (*(volatile unsigned int*)0x44E35048)
-#define WDT_WWPS              (*(volatile unsigned int*)0x44E35034)
-
 // As funções
 void disable_wdt(void){
   	WDT_WSPR = 0xAAAA;
