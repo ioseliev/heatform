513c302cf8a99d5e470e1dc0e4636415d25b40e8
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 10:22:28 -0300

    Update gpio.c

--- src/gpio.c
+++ src/gpio.c
@@ -2,13 +2,13 @@
 
 // Todos os endereços úteis do módulo de gpio
 #define GPIO1			0x4804C000		// GPIO1 registers
-#define GPIO_IRQSTATUS		(*(volatile unsigned int *)GPIO1 + 0x2C)
-#define GPIO_OE			(*(volatile unsigned int *)GPIO1 + 0x134)
-#define GPIO_CLEARDATAOUT	(*(volatile unsigned int *)GPIO1 + 0x190)
-#define GPIO_SETDATAOUT		(*(volatile unsigned int *)GPIO1 + 0x194)
-#define GPIO_IRQSTATUS_SET	(*(volatile unsigned int *)GPIO1 + 0x34)
-#define GPIO_IRQSTATUS_CLR	(*(volatile unsigned int *)GPIO1 + 0x3C)
-#define GPIO_RISINGDETECT	(*(volatile unsigned int *)GPIO1 + 0x148)
+#define GPIO_IRQSTATUS		(*(volatile unsigned int *)(GPIO1 + 0x2C))
+#define GPIO_OE			(*(volatile unsigned int *)(GPIO1 + 0x134))
+#define GPIO_CLEARDATAOUT	(*(volatile unsigned int *)(GPIO1 + 0x190))
+#define GPIO_SETDATAOUT		(*(volatile unsigned int *)(GPIO1 + 0x194))
+#define GPIO_IRQSTATUS_SET	(*(volatile unsigned int *)(GPIO1 + 0x34))
+#define GPIO_IRQSTATUS_CLR	(*(volatile unsigned int *)(GPIO1 + 0x3C))
+#define GPIO_RISINGDETECT	(*(volatile unsigned int *)(GPIO1 + 0x148))
 
 // As funções propriamente ditas
 void gpio1_rising_detect(int pin){
