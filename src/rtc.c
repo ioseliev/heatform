995e3dcccde7cb940254765eeb82df0041bb0d0e
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 08:11:33 -0300

    Update rtc.c

--- src/rtc.c
+++ src/rtc.c
@@ -1,3 +1,5 @@
+#include "rtc.h"
+
 #define CM_RTC			0x44E00800					// Clock Module RTC Registers
 #define CM_RTC_RTC_CLKCTRL	(*(volatile unsigned int *)CM_RTC + 0x00)
 #define CM_RTC_CLKSTCTRL	(*(volatile unsigned int *)CM_RTC + 0x04)
@@ -12,16 +14,6 @@
 #define RTC_OSC_REG		(*(volatile unsigned int *)RTC + 0x54)		// Oscillator Register
 #define KICK0R			(*(volatile unsigned int *)RTC + 0x6C)		// Kick 0 Register (Write Protect)
 #define KICK1R			(*(volatile unsigned int *)RTC + 0x70)		// Kick 1 Register (Write Protect)
-
-// Os protótipos das funções
-void define_second(char *n);
-void define_minute(char *n);
-void define_hour(char *n);
-void rtc_interrupt_enable(void);
-void rtc_external_clock(void);
-void start_rtc(void);
-void rtc_write_protection_disable(void);
-void rtc_clock_enable(void);
 
 // As funções
 void define_hour(char *n){
