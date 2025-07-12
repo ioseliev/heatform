7447d244aa653d3bab7192ede639633b3aaa3c68
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 10:24:44 -0300

    Update intc.c

--- src/intc.c
+++ src/intc.c
@@ -1,10 +1,10 @@
 #include "intc.h"
 
 #define INTC			0x48200000		// Interrupt Controller Registers
-#define INTC_SIR_IRQ		(*(volatile unsigned int *)INTC + 0x40)
-#define INTC_CONTROL		(*(volatile unsigned int *)INTC + 0x48)
-#define INTC_MIR_CLEAR2		(*(volatile unsigned int *)INTC	+ 0xC8)
-#define INTC_MIR_CLEAR3		(*(volatile unsigned int *)INTC + 0xE8)
+#define INTC_SIR_IRQ		(*(volatile unsigned int *)(INTC + 0x40))
+#define INTC_CONTROL		(*(volatile unsigned int *)(INTC + 0x48))
+#define INTC_MIR_CLEAR2		(*(volatile unsigned int *)(INTC + 0xC8))
+#define INTC_MIR_CLEAR3		(*(volatile unsigned int *)(INTC + 0xE8))
 
 // As funções
 void intc_interrupt_rtc_accept(void){
