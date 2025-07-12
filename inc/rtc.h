fc9ee6266e3cef30ea97be4a699f6264de4d3c8b
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 11:33:35 -0300

    Update rtc.h

--- inc/rtc.h
+++ inc/rtc.h
@@ -1,6 +1,21 @@
 #ifndef RTC_H
 #define RTC_H
 
+#define CM_RTC			0x44E00800					// Clock Module RTC Registers
+#define CM_RTC_RTC_CLKCTRL	(*(volatile unsigned int *)(CM_RTC + 0x00))
+#define CM_RTC_CLKSTCTRL	(*(volatile unsigned int *)(CM_RTC + 0x04))
+
+#define RTC			0x44E3E000					// RTC Registers
+#define SECONDS_REG		(*(volatile unsigned int *)(RTC + 0x00))		// Seconds Register
+#define MINUTES_REG		(*(volatile unsigned int *)(RTC + 0x04))		// Minutes Register
+#define HOURS_REG		(*(volatile unsigned int *)(RTC + 0x08))		// Hours Register
+#define RTC_CTRL_REG		(*(volatile unsigned int *)(RTC + 0x40))		// Control Register
+#define RTC_STATUS_REG		(*(volatile unsigned int *)(RTC + 0x44))		// Status Register
+#define RTC_INTERRUPTS_REG	(*(volatile unsigned int *)(RTC + 0x48))		// Interrupt Enable Register
+#define RTC_OSC_REG		(*(volatile unsigned int *)(RTC + 0x54))		// Oscillator Register
+#define KICK0R			(*(volatile unsigned int *)(RTC + 0x6C))		// Kick 0 Register (Write Protect)
+#define KICK1R			(*(volatile unsigned int *)(RTC + 0x70))		// Kick 1 Register (Write Protect)
+
 // Os protótipos das funções
 void define_second(char *n);
 void define_minute(char *n);
