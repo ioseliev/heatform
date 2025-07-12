b4bb1b1517acf8952b930ec8efb33e1eed3e5bc8
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 08:05:30 -0300

    Update intc.h

--- inc/intc.h
+++ inc/intc.h
@@ -1,1 +1,9 @@
+#ifndef INTC_H
+#define INTC_H
 
+// Protótipo das funções
+void intc_interrupt_rtc_accept(void);
+void intc_interrupt_i2c_accept(void);
+void intc_interrupt_gpio_accept(void);
+
+#endif
