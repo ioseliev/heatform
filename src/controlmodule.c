e1f8de2158bbfbdae3012d3c416113e4f52fa5ee
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2025-07-12 10:48:47 -0300

    Update controlmodule.c

--- src/controlmodule.c
+++ src/controlmodule.c
@@ -1,10 +1,10 @@
 #include "controlmodule.h"
 
 #define CONTROL_MODULE	0x44E10000						// Control Module Registers
-#define CONF_UART0_RXD	(*(volatile unsigned int *)(CONTROL_MODULE 0x970))
-#define CONF_UART0_TXD	(*(volatile unsigned int *)(CONTROL_MODULE 0x974))
-#define CONF_MII1_CRS	(*(volatile unsigned int *)(CONTROL_MODULE 0x90C))
-#define CONF_MII1_RX_ER	(*(volatile unsigned int *)(CONTROL_MODULE 0x910))
+#define CONF_UART0_RXD	(*(volatile unsigned int *)(CONTROL_MODULE + 0x970))
+#define CONF_UART0_TXD	(*(volatile unsigned int *)(CONTROL_MODULE + 0x974))
+#define CONF_MII1_CRS	(*(volatile unsigned int *)(CONTROL_MODULE + 0x90C))
+#define CONF_MII1_RX_ER	(*(volatile unsigned int *)(CONTROL_MODULE + 0x910))
 
 // As funções
 void i2c1_pin_mux(void){
