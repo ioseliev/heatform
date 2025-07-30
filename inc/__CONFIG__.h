7c6450ad56eb71e8414a7b806edfc5019804ea04
ioseliev <ioseliev@pm.me>
2025-07-30 00:42:51 +0000

    Add config file

---
+++ inc/__CONFIG__.h
@@ -0,0 +1,12 @@
+#ifndef __CONFIG__H
+#define __CONFIG__H
+
+
+// Temperature to be monitored, in Celsius
+#define TARGET_TEMP 24.0
+
+// Target temperature type, either NONE, LOW (for a lower bound)
+// or HIGH (for an upper bound)
+#define TARGET_TYPE HIGH
+
+#endif
