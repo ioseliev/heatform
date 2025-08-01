4ca872acc15f6b549fd70243a46e002762bcc7ba
ioseliev <ioseliev@pm.me>
2025-08-01 02:02:24 +0000

    Fix off-by-one and move CRLF to the very end of the canvas line

--- src/canvas.c
+++ src/canvas.c
@@ -3,25 +3,23 @@
 
 
 void canvas_init(uint8_t *buffer) {
-  const uint8_t FORMAT[] = " [00:00:00] Obj:        Amb:        Min:        Max:      ";
+  const uint8_t FORMAT[] = "[00:00:00] Obj:        Amb:        Min:        Max:       ";
 
   memset(buffer, ' ', CANVAS_LINE_LENGTH);
   memcpy(buffer, FORMAT, sizeof(FORMAT) - 1);
 
-  buffer[CANVAS_LINE_LENGTH] = '\0';
+  buffer[CANVAS_LINE_LENGTH - 1] = '\0';
 }
 
 void canvas_update(uint8_t *buffer, const state *state, bool skip) {
-  const size_t FMT_CRLF = 0;
-  const size_t FMT_HOURS = 2;
-  const size_t FMT_MINUTES = 5;
-  const size_t FMT_SECONDS = 8;
-  const size_t FMT_OBJ = 17;
-  const size_t FMT_AMB = 29;
-  const size_t FMT_MIN = 41;
-  const size_t FMT_MAX = 53;
-
-  buffer[FMT_CRLF] = (skip == true ? '\n' : '\r');
+  const size_t FMT_HOURS = 1;
+  const size_t FMT_MINUTES = 4;
+  const size_t FMT_SECONDS = 7;
+  const size_t FMT_OBJ = 16;
+  const size_t FMT_AMB = 28;
+  const size_t FMT_MIN = 40;
+  const size_t FMT_MAX = 52;
+  const size_t FMT_CRLF = 62;
 
   write_bcd(buffer + FMT_HOURS, state->hours);
   write_bcd(buffer + FMT_MINUTES, state->minutes);
@@ -30,4 +28,6 @@ void canvas_update(uint8_t *buffer, const state *state, bool skip) {
   write_float(buffer + FMT_OBJ, state->temp);
   write_float(buffer + FMT_MIN, state->temp_min);
   write_float(buffer + FMT_MAX, state->temp_max);
+
+  buffer[FMT_CRLF] = (skip == true ? '\n' : '\r');
 }
